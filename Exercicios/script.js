6c8fb572d7ab933aebe767b95557667412802d48
Leonardo Moura de Sousa <leonardo.sousa@sptech.school>
2026-02-20 14:04:05 -0300

    Adicionando os arquivos de exercícios e listas passadas

---
+++ Exercicios/script.js
@@ -0,0 +1,51 @@
+let textoDosNums = document.querySelector("#num");
+let num1 = document.querySelector("#um");
+let num2 = document.querySelector("#2");
+let num3 = document.querySelector("#3");
+let num4 = document.querySelector("#4");
+let num5 = document.querySelector("#5");
+let num6 = document.querySelector("#6");
+let num7 = document.querySelector("#7");
+let num8 = document.querySelector("#8");
+let num9 = document.querySelector("#9");
+let num0 = document.querySelector("#0");
+
+function numum() {
+  textoDosNums.innerHTML = +String(numum.value);
+}
+
+function num2() {
+  textoDosNums.innerHTML = +String(num1.value);
+}
+
+function num3() {
+  textoDosNums.innerHTML = +String(num1.value);
+}
+
+function num4() {
+  textoDosNums.innerHTML = +String(num1.value);
+}
+
+function num5() {
+  textoDosNums.innerHTML = +String(num1.value);
+}
+
+function num6() {
+  textoDosNums.innerHTML = +String(num1.value);
+}
+
+function num7() {
+  textoDosNums.innerHTML = +String(num1.value);
+}
+
+function num8() {
+  textoDosNums.innerHTML = +String(num1.value);
+}
+
+function num9() {
+  textoDosNums.innerHTML = +String(num1.value);
+}
+
+function num0() {
+  textoDosNums.innerHTML = +String(num1.value);
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Aulas_Algoritmo
+
+Estarei postando todos os exercícios feitos nas aulas de Algoritmo com o JP.
+Separadamente por aulas/listas.
